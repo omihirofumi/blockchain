38d0646190fb0e5958ad76d144d7f395bf399edc
hirofumi omi <omihirofumi@gmail.com>
2023-06-14 22:19:35 +0900

    modify hash string -> [32]byte

--- internal/block/blockchain.go
+++ internal/block/blockchain.go
@@ -1,6 +1,8 @@
 package block
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"time"
 )
 
@@ -8,17 +10,22 @@ import (
 type Block struct {
 	timestamp    int64
 	nonce        int
-	previousHash string
+	previousHash [32]byte
 	transaction  []string
 }
 
 // NewBlock はブロックを生成する
-func NewBlock(nonce int, previousHash string) *Block {
+func NewBlock(nonce int, previousHash [32]byte) *Block {
 	return &Block{
 		timestamp:    time.Now().UnixNano(),
 		nonce:        nonce,
 		previousHash: previousHash,
 	}
+}
+
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
 }
 
 // Blockchain はブロックチェーンを表す
@@ -29,14 +36,21 @@ type Blockchain struct {
 
 // NewBlockChain はgenesisブロックを追加したブロックチェーンを生成する
 func NewBlockChain() *Blockchain {
+	// genesis block
+	b := &Block{}
 	bc := &Blockchain{}
-	bc.CreateBlock(0, "genesis hash")
+	bc.CreateBlock(0, b.Hash())
 	return bc
 }
 
 // CreateBlock はブロックを生成しブロックチェーンへ追加する
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
+}
+
+// LastBlock は前回のブロックを返す
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
--- internal/block/blockchain_test.go
+++ internal/block/blockchain_test.go
@@ -8,15 +8,14 @@ import (
 func TestBlockchain(t *testing.T) {
 	bc := NewBlockChain()
 	require.Equal(t, 0, bc.chain[0].nonce)
-	require.Equal(t, "genesis hash", bc.chain[0].previousHash)
 	require.Equal(t, 1, len(bc.chain))
 
-	b1 := bc.CreateBlock(1, "second hash")
+	b1 := bc.CreateBlock(1, bc.LastBlock().Hash())
 	require.Equal(t, 1, b1.nonce)
-	require.Equal(t, "second hash", b1.previousHash)
-	b2 := bc.CreateBlock(2, "third hash")
+	require.Equal(t, bc.LastBlock().Hash(), b1.previousHash)
+	b2 := bc.CreateBlock(2, bc.LastBlock().Hash())
 	require.Equal(t, 2, b2.nonce)
-	require.Equal(t, "third hash", b2.previousHash)
+	require.Equal(t, bc.LastBlock().Hash(), b2.previousHash)
 
 	require.Equal(t, 3, len(bc.chain))
 }
